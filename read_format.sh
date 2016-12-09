18bcd815c52d44d1df38efaa3dec6a6bcfa6b7bf
GaoleiBai <937294944@qq.com>
2016-12-09 09:39:32 +0800

    Create read_format.sh

---
+++ read_format.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#example2  for  read
+echo "Input  date  with  format  yyyy  mm dd: "
+read  year  month  day
+echo  "Today  is  $year/$month/$day,  right?"
+echo  "Press  any  key  to  confirm  and  continue"
+read  answer
+echo "I  know  the  date,  bye!"
+
